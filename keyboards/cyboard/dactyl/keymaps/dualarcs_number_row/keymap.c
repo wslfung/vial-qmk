d48708bf58143717740055d41749dd0d128ddad5
wslfung <wilson.sl.fung@gmail.com>
2023-05-06 10:00:05 -0700

    updated print screen and caps lock placement

--- keyboards/cyboard/dactyl/keymaps/dualarcs_number_row/keymap.c
+++ keyboards/cyboard/dactyl/keymaps/dualarcs_number_row/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ),
 
     [0] = LAYOUT_dualarcs_number_row(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_CAPS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_BSPC, LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), KC_F, KC_G, KC_H, KC_J, RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_NO, KC_NO, KC_NO, KC_NO, TD(3), TD(1), TD(2), KC_DEL, KC_SPC, KC_ENT, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCTL, KC_LALT, TD(1), KC_RCTL
+        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSLS, KC_BSPC, LCTL_T(KC_A), LGUI_T(KC_S), LALT_T(KC_D), KC_F, KC_G, KC_H, KC_J, RALT_T(KC_K), RGUI_T(KC_L), RCTL_T(KC_SCLN), KC_QUOT, KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_NO, KC_NO, KC_NO, KC_NO, MO(3), MO(1), MO(2), KC_DEL, KC_SPC, KC_ENT, KC_NO, KC_NO, KC_NO, KC_NO, KC_LCTL, KC_LALT, MO(1), KC_RCTL
     ),
 
     // [1] = LAYOUT_dualarcs_number_row(
@@ -29,11 +29,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // ),
 
     [1] = LAYOUT_dualarcs_number_row(
-        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_RGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_TRNS, KC_GRV, KC_TRNS, KC_TRNS, KC_PSCR, KC_TRNS, KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_DEL, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_RGUI, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_MINS, KC_EQL, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [2] = LAYOUT_dualarcs_number_row(
-        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_PSLS, KC_PAST, KC_PMNS, KC_NO, KC_TRNS, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_TRNS, KC_TRNS, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_TRNS, KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_P0, KC_P4, KC_P5, KC_P6, KC_PDOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1, KC_P2, KC_P3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_PSLS, KC_PAST, KC_PMNS, KC_CAPS, KC_TRNS, KC_PGUP, KC_HOME, KC_UP, KC_END, KC_TRNS, KC_TRNS, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_TRNS, KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_P0, KC_P4, KC_P5, KC_P6, KC_PDOT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_P1, KC_P2, KC_P3, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
 
     [3] = LAYOUT_dualarcs_number_row(
@@ -168,7 +168,8 @@ rgb_matrix_indicators_user(void) {
         case 1:
             //left
             rgb_matrix_set_color(5, RGB_CYAN);
-            rgb_matrix_set_color(31, RGB_CORAL);    
+            rgb_matrix_set_color(31, RGB_CORAL);  
+            rgb_matrix_set_color(13, RGB_CORAL);   
             //right
             rgb_matrix_set_color(59, RGB_RED);
             rgb_matrix_set_color(52, RGB_RED);
@@ -178,10 +179,10 @@ rgb_matrix_indicators_user(void) {
             rgb_matrix_set_color(65, RGB_PURPLE);
             rgb_matrix_set_color(70, RGB_CORAL);
             rgb_matrix_set_color(71, RGB_CORAL);
-            rgb_matrix_set_color(40, RGB_YELLOW);
-            rgb_matrix_set_color(51, RGB_YELLOW);
-            rgb_matrix_set_color(57, RGB_YELLOW);
-            rgb_matrix_set_color(63, RGB_YELLOW);
+            rgb_matrix_set_color(40, RGB_MAGENTA);
+            rgb_matrix_set_color(51, RGB_MAGENTA);
+            rgb_matrix_set_color(57, RGB_MAGENTA);
+            rgb_matrix_set_color(63, RGB_MAGENTA);
             rgb_matrix_set_color(76, RGB_CYAN);
             break;
         case 2:
@@ -211,6 +212,7 @@ rgb_matrix_indicators_user(void) {
             rgb_matrix_set_color(66, RGB_PURPLE);
             rgb_matrix_set_color(72, RGB_PURPLE);
             rgb_matrix_set_color(71, RGB_PURPLE);
+            rgb_matrix_set_color(78, RGB_TEAL);
             break;
         case 3:
             // left
